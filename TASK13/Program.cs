7623c66fac2bb5e279b13b3599618a587793c1dc
abdelrhman kamal <134321565+Abdelrhmanmattar@users.noreply.github.com>
2025-01-02 15:59:31 +0200

    Add files via upload

---
+++ TASK13/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DAY13
+{
+    class Club
+    {
+        public void AddInClub()
+        {
+            Console.WriteLine("Employee added to the club");
+        }
+    }
+    class SocialInsurance
+    {
+        public void BeginSocialInsurance()
+        {
+            Console.WriteLine("Employee's social insurance begin");
+        }
+    }
+    public class employee
+    {
+        private Club club;
+        private SocialInsurance socialInsurance;
+        public employee()
+        {
+            this.club = new Club();
+            this.socialInsurance = new SocialInsurance();
+        }
+        public void Add()
+        {
+            Console.WriteLine("Employee added to company");
+            club.AddInClub();
+            socialInsurance.BeginSocialInsurance();
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            employee emp = new employee();
+            emp.Add();
+        }
+    }
+
+}
